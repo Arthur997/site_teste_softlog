73ea43bb620abe2f3c428d8a7aa7938f22d68205
Arthur Frota <arthur.frota@hotmail.com>
2020-04-03 23:46:09 -0300

    site test

---
+++ run.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/painel")
+def painel():
+    return render_template('painel.html')
+
+@app.route("/form-cliente")
+def form_cliente():
+    return render_template('form-cliente.html')
+
+@app.route("/form-filial")
+def form_filial():
+    return render_template('form-filial.html')
+
+if __name__ == "__main__":
+    app.run()
+
+
+
